5317ab00e9ff0df4b2371bf7fcdf6f2b61c1f493
head-node <76886903+head-node@users.noreply.github.com>
2021-03-20 22:01:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,73 @@
 import React from "react";
-import "./../styles/App.css";
+import "./styles.css";
+import TodoList from "./TodoList";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+export default function App() {
+  const [data, setData] = React.useState("");
+  const [items, setItems] = React.useState([]);
+  const listOfItems = () => {
+    if (data.length > 0) {
+      setItems((oldItems) => {
+        //since the function usually deals with the default value, so here the parameter is the default value
+        return [...oldItems, data]; // oldItems would be referring to the default variable, i.e is an array
+      });
+      setData("");
+    }
+  };
+  const deleteItems = (id) => {
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
+        return index !== id;
+      });
+    });
+  };
+  const updateItem = (idx, value) => {
+    if(value!==""){
+    items[idx] = value;
+    setItems([...items]);
+  }
+};
+
+  const handleClick = (id) => {
+    console.log(id);
+  };
+
+  return (
+    <>
+      <div className="main">
+        <div className="center_div">
+          <br />
+          <h1>TODO LIST</h1>
+          <br />
+          <div className="inpb">
+            <input
+              type="text"
+              value={data}
+              placeholder="Add an Item"
+              onChange={(event) => {
+                setData(event.target.value);
+              }}
+            />
+            <button className="btn" onClick={listOfItems}>
+              +
+            </button>
+          </div>
+          <ol>
+            {items.map((item, index) => {
+              return (
+                <TodoList
+                  key={index}
+                  id={index}
+                  item={item}
+                  onSelect={deleteItems}
+                  handleClick={handleClick}
+                  updateItem={updateItem}
+                />
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
 }
-
-
-export default App;
